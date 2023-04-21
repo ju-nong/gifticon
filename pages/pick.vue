c11f3f2e966b9986842bbf7bc98724eabb593e8c
이준용 <iiiuooyoo@gmail.com>
2023-04-21 17:20:32 +0900

    feat : 임시

--- pages/pick.vue
+++ pages/pick.vue
@@ -1,8 +1,19 @@
 <template>
     <h1 class="title">와 축하해!</h1>
-    <div class="pick-container">어서오고</div>
+    <div class="pick-container">
+        <img :src="`/images/${data.list[pick]}`" alt="" />
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { storeToRefs } from "pinia";
+import { dbStore, userStore } from "~/stores";
+
+const db = dbStore();
+const { data } = storeToRefs(db);
+
+const user = userStore();
+const { pick } = storeToRefs(user);
+</script>
 
 <style lang="scss"></style>
