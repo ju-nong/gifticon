4768e7adcb79c4f07ed9bff8e8505fda0e8cf37a
이준용 <iiiuooyoo@gmail.com>
2023-04-24 16:21:58 +0900

    feat : meta, favicon 수정

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,20 @@
 <template>
+    <Head>
+        <Title>생일선물 뽑기</Title>
+        <Meta name="theme-color" content="rgb(26 136 233)" />
+        <Meta name="description" content="이준용이 주는 생일선물!" />
+        <Meta
+            name="keywords"
+            content="이준용, 오아리, 생일선물, 기프티콘, gifticon"
+        />
+        <Meta name="author" content="이준용" />
+        <Meta content="생일선물 뽑기" property="og:title" />
+        <Meta content="/ari.png" property="og:image" />
+        <Meta content="website" property="og:type" />
+        <Meta content="choose-gifticon.vercel.app" property="og:site_name" />
+        <Meta content="//choose-gifticon.vercel.app/" property="og:url" />
+        <Link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+    </Head>
     <SnackbarContainer />
     <Modal />
     <main class="w-full h-full flex justify-center items-center p-[10rem]">
