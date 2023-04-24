1dff2b5658d6569bb258865cb1c214d6a28ef265
이준용 <iiiuooyoo@gmail.com>
2023-04-24 12:41:15 +0900

    feat : Exam Header 컴포넌트

--- components/exam/Header.vue
+++ components/exam/Header.vue
@@ -1,5 +1,16 @@
 <template>
-    <ul class="w-full"></ul>
+    <div class="exam-header-container w-full flex items-center justify-center">
+        <ul class="flex items-center justify-center gap-x-10 relative">
+            <li
+                v-for="(_, index) in config"
+                :key="index"
+                class="font-semibold text-center"
+                :class="index === step ? 'active' : ''"
+            >
+                {{ index + 1 }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
@@ -7,7 +18,37 @@ import { storeToRefs } from "pinia";
 import { examStore } from "~/stores";
 
 const exam = examStore();
-const { step } = storeToRefs(exam);
+const { step, config } = storeToRefs(exam);
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.exam-header-container {
+    ul {
+        &::before {
+            content: "";
+            position: absolute;
+            height: 2px;
+            background-color: rgb(26 136 233);
+            top: 50%;
+            left: 0;
+            width: 100%;
+            z-index: -1;
+        }
+        li {
+            color: rgb(26 136 233);
+            border: 2px solid rgb(26 136 233);
+            background-color: #fff;
+            border-radius: 100%;
+            font-size: 20px;
+            width: 36px;
+            line-height: 34px;
+
+            &.active {
+                color: #fff;
+                background-color: rgb(26 136 233);
+                font-weight: bold;
+            }
+        }
+    }
+}
+</style>
