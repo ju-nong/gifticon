faa9ec5d7c91d31c45c9c5de7034a84f303d085f
이준용 <iiiuooyoo@gmail.com>
2023-06-14 11:25:01 +0900

    faet : Admin / 탭구현 프로토타입

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,9 +1,43 @@
 <template>
     <h1 class="title">어 준용이 어서오고</h1>
+    <div class="admin-container">
+        <button
+            v-for="(item, index) in config.list"
+            :key="index"
+            @click="config.active = index"
+        >
+            {{ item.icon }}
+        </button>
+        <component :is="activeComponent" />
+    </div>
 </template>
 
 <script setup>
-import { adminStore, userStore } from "~/stores";
+// 핸들링하기 위함
+const config = reactive({
+    list: [
+        {
+            icon: "📷",
+            name: "camera",
+        },
+        {
+            icon: "📝",
+            name: "exam",
+        },
+    ],
+    active: 0,
+});
+
+// 보여줄 컴포넌트를 담음
+const components = reactive({
+    camera: resolveComponent("AdminCamera"),
+    exam: resolveComponent("AdminExam"),
+});
+
+// 현재 Active된 Component 뱉음
+const activeComponent = computed(
+    () => components[config.list[config.active].name],
+);
 </script>
 
 <style lang="scss"></style>
