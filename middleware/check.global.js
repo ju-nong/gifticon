8ed445ac253367566f231b3a000c1c2f9fd88828
이준용 <iiiuooyoo@gmail.com>
2023-08-07 11:52:21 +0900

    feat : middleware 주석 수정

--- middleware/check.global.js
+++ middleware/check.global.js
@@ -1,17 +1,17 @@
 import { userStore, pageStore } from "~/stores";
 
 export default defineNuxtRouteMiddleware((to) => {
-    const user = userStore();
     const page = pageStore();
 
     page.toggleLoad();
 
     if (to.name !== "index" && to.name !== "board") {
+        const user = userStore();
+
+        // 메인 페이지나 방명록 페이지가 아니면서 로그인을 하지 않은 상태일 경우
         if (!user.getLogin) {
-            // 메인 페이지가 아니면서 현재 잘못된 접근일 경우
             page.setBlock(true); // 잘못된 접근 Flag 변수를 True로 변경
-            // return navigateTo("/"); // 메인으로 이동    이거 넣으면 다른 게 작동이 안 되네
+            // return navigateTo("/"); // 메인으로 이동
         }
-    } else {
     }
 });
